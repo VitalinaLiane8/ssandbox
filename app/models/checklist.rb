a684fd1fd43b1feb98a0b9bc1401c825fa1a63fe
AskanHolaAE17 <be.in.pair@gmail.com>
2020-02-24 19:00:25 +0200

    Add client and server validation for Checklist

--- app/models/checklist.rb
+++ app/models/checklist.rb
@@ -1,4 +1,8 @@
 # frozen_string_literal: true
 
 class Checklist < ApplicationRecord
+
+  validates :title,         presence: true, length: { maximum: 40 }
+  validates :description,   presence: true
+
 end
