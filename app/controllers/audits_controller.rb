89aba7a305ffd3b3c877006a47f9b76512c72cfb
AskanHolaAE17 <be.in.pair@gmail.com>
2020-02-26 21:15:15 +0200

    Destroy method for audit

--- app/controllers/audits_controller.rb
+++ app/controllers/audits_controller.rb
@@ -3,8 +3,21 @@
 # Controller for user sees the list of audits
 class AuditsController < ApplicationController
 
+  before_action :set_checklist, only: [:destroy]
+
   def index
     @audits = Audit.page(params[:page]).per(10)
   end 
   
+  def destroy
+    @audit.destroy
+    redirect_to audits_url, notice: 'Audit was successfully destroyed.'
+  end
+  
+  
+  private
+    def set_audit
+      @audit = Audit.find(params[:id])
+    end 
+        
 end  
